30ed56f622dda7855834003bcea56a092af9efe9
Ben <yacoobbenyamain@gmail.com>
2023-07-12 15:38:54 -0400

    Adding scripts for agent training and its limited behavior (put intently in place to avoid exposure)

--- Assets/Scripts/MoveToTargetAgent.cs
+++ Assets/Scripts/MoveToTargetAgent.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
-public class MoveToTargetAgent : MonoBehaviour
+public class MoveToTargetAgent : Agent
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private Transform target;
+
+    public override void OnEpisodeBegin() {
+        transform.position = new Vector3(Random.Range(-3.5f, -1.5f), Random.Range(-3.5f, 3.5f));
+        target.position = new Vector3(Random.Range(-1.5f, -3.5f), Random.Range(-3.5f, 3.5f));
+    }
+    
+    public override void CollectObservations(VectorSensor sensor) {
+        sensor.AddObservation((Vector2)transform.position);
+        sensor.AddObservation((Vector2)target.position);
+    }
+    
+    public override void OnActionReceived(ActionBuffers actions) {
+        float moveX = actions.ContinuousActions[0];
+        float moveY = actions.ContinuousActions[1];
+        float movementSpeed = 5f;
+
+        // Restraint to the single environment and not others (we will have multiple environments)
+        transform.localPosition += new Vector3(moveX, moveY) * Time.deltaTime * movementSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.TryGetComponent(out Target target))
+        {
+            AddReward(10f);
+            EndEpisode();
+        } else if (other.TryGetComponent(out Wall wall)) {
+            AddReward(-2f);
+            EndEpisode();
+        }
     }
 }
